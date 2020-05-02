e26a989050452a0631140a0073f4b9be06bc778e
prueba <42043332+dacardo4@users.noreply.github.com>
2020-05-01 20:02:25 -0500

    querys create fisrt 6 tables

---
+++ db/querys.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `bovxuaqnosdh4yledh9c`.`product` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `productName` VARCHAR(45) NOT NULL,
+  `idUser` INT NULL,
+  `idCategory` INT NULL,
+  `idSubcategory` INT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `bovxuaqnosdh4yledh9c`.`market` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `marketName` VARCHAR(45) NOT NULL,
+  `marketAddress` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `bovxuaqnosdh4yledh9c`.`prices` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `price` INT NOT NULL,
+  `idProduct` INT NULL,
+  `idMarket` INT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `bovxuaqnosdh4yledh9c`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `categoryName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `bovxuaqnosdh4yledh9c`.`subcategory` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `subcategoryName` VARCHAR(45) NOT NULL,
+  `idCategory` INT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `bovxuaqnosdh4yledh9c`.`quantity` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `quantity` INT NOT NULL,
+  `idProduct` INT NULL,
+  `idUser` INT NULL,
+  PRIMARY KEY (`id`));
+
